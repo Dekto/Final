c5b231dadfe15808ead73179960f28b1eb0464e3
Dekto <37834268+Dekto@users.noreply.github.com>
2018-11-18 21:36:21 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
   root to: 'jobs#index'
-
+  get "/:page" => "static#show" 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
