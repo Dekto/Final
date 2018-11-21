6c15a7495e9fe9385bc03e95c8a01ce52e5b17ff
Dekto <37834268+Dekto@users.noreply.github.com>
2018-11-21 20:19:30 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   end
   root to: 'jobs#index'
   get "/pages/:page" => "pages#show"
+  get "/:page" => "static#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
